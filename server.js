1167e1799e680fcac8481ca2dda614328db2b126
Steven Burnett <sburnett@instructure.com>
2016-06-03 16:30:31 -0600

    fix routing problem with static assets

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 
 if(process.env.NODE_ENV === 'development'){
-  app.use(express.static('/public'));
+  app.use(express.static('public'));
 }
 
 app.listen(3000, '0.0.0.0', function () {
